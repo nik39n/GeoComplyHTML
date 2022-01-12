cf6b06ee77c38dfeaf37e67c508ed763dbe652ec
nik39n <40721920+nik39n@users.noreply.github.com>
2022-01-12 16:41:13 +0200

    php_6_8

--- homework/php/homework/html/lesson9/index3.php
+++ homework/php/homework/html/lesson9/index3.php
@@ -1,10 +1,10 @@
 <?php
-$i=1;
-setcookie('count',$i,time()+36000);
-if (isset($_COOKIE['count'])){
-    echo "Вы были " . $_COOKIE['count'];
-    $i = ++$_COOKIE['count'];
+setcookie('count',1,time()+36000);
+$i = ++$_COOKIE['count'];
+if ($_COOKIE['count']>1){
     setcookie("count", $i,time()+36000);
-} else {
+    echo "Вы были " . $_COOKIE['count']. "раз";
+} elseif($_COOKIE['count']==1) {
     echo "вы не были здесь";
 }
+
