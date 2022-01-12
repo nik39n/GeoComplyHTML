a369105dfd0a2d13e76134eb5333765afd580b6b
nik39n <40721920+nik39n@users.noreply.github.com>
2022-01-12 17:38:18 +0200

    php_partials

---
+++ homework/php/homework/html/partials/about.php
@@ -0,0 +1,7 @@
+<?php
+$companyName = 'apple';
+?>
+<?php include_once './header.php';?>
+<h1>About us</h1>
+
+<?php include_once './footer.php';?>
---
+++ homework/php/homework/html/partials/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$companyName = 'apple';
+?>
+<?php include_once './header.php';?>
+<?php include_once './location.php';?>
+<h1>Welcome to my cool website</h1>
+<?php include_once './footer.php';?>
